10dd251867a653e493c2b9ad699bd2a915e36a37
TangHaiyin1 <tongtranhung1@gmail.com>
2020-08-06 15:47:08 +0700

    zero5

---
+++ menudacap/index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("drop1").classList.toggle("show");
+}
+function myFunction1() {
+    document.getElementById("drop2").classList.toggle("show1");
+}
